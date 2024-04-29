b3dd848d72084543e3d806cac2789e5385f3aab3
Suhas Srinivasa Reddy <susr2271@aesd-students.colorado.edu>
2024-04-29 08:26:05 +0000

    Sprint-3: Inegrated LED and Motor

--- base_external/package/display/display.mk
+++ base_external/package/display/display.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 
-DISPLAY_VERSION = 65fdc2c36efc50c12dbbbd53bc5c89f6b754a715
+DISPLAY_VERSION = aed012aa2ce86f636b78e34a4d314a251163f68a
 DISPLAY_SITE = git@github.com:krishnasuhagiya29/final-project-assignment-krishna-suhas.git
 DISPLAY_SITE_METHOD = git
 DISPLAY_GIT_SUBMODULES = YES
@@ -17,7 +17,7 @@ endef
 
 # Adding I2C display application
 define DISPLAY_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/display/display $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/display/display.so $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
--- base_external/package/motor/motor.mk
+++ base_external/package/motor/motor.mk
@@ -6,16 +6,20 @@
 ##############################################################
 
 
-MOTOR_VERSION = 65fdc2c36efc50c12dbbbd53bc5c89f6b754a715
+MOTOR_VERSION = aed012aa2ce86f636b78e34a4d314a251163f68a
 MOTOR_SITE = git@github.com:krishnasuhagiya29/final-project-assignment-krishna-suhas.git
 MOTOR_SITE_METHOD = git
 MOTOR_GIT_SUBMODULES = YES
 
+# Set CFLAGS to include the display directory
+TARGET_CFLAGS += -I$(@D)/display
+
+# Make sure that the display object files are compiled
 define MOTOR_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/motor all
+    $(MAKE) CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" -C $(@D)/motor all
 endef
 
-# Adding pwm motor bash script and application
+# Adding pwm motor bash script and speed control application
 define MOTOR_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/motor/motor.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/motor/speed_control $(TARGET_DIR)/usr/bin
